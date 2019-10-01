a413c232dee8c2e5b965b3b852f9ca28964c7507
sepiariver <info@sepiariver.com>
2019-10-01 15:09:10 -0700

    couple assertions

--- test/cases/OAuth2ServerTest.php
+++ test/cases/OAuth2ServerTest.php
@@ -21,4 +21,21 @@ class OAuth2ServerTest extends TestCase
     {
         $this->assertTrue($this->oauth2server instanceof \OAuth2Server);
     }
+    public function testCreateServer()
+    {
+        $server = $this->oauth2server->createServer();
+        $this->assertTrue($server instanceof \OAuth2\Server);
+        $diff = array_diff_key(['authorization_code' => null, 'refresh_token' => null, 'client_credentials' => null], $server->getGrantTypes());
+        $this->assertEquals([], $diff);
+    }
+    public function testCreateRequest()
+    {
+        $request = $this->oauth2server->createRequest();
+        $this->assertTrue($request instanceof \OAuth2\Request);
+        $this->assertEquals([], $request->request);
+
+        $request->initialize(['foo' => 'bar'], ['fooz' => 'baz']);
+        $this->assertEquals('bar', $request->query('foo'));
+        $this->assertEquals('baz', $request->request('fooz'));
+    }
 }
